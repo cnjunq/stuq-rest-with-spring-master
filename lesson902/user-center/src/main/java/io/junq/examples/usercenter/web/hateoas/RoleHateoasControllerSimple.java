d0275ffc7eec6dd4528eeaca49957ff61b04c4fb
liujunqiang <liujunqiang@xunlei.com>
2017-12-26 00:31:20 +0800

    new: lesson 902 initial code

---
+++ lesson902/user-center/src/main/java/io/junq/examples/usercenter/web/hateoas/RoleHateoasControllerSimple.java
@@ -0,0 +1,32 @@
+package io.junq.examples.usercenter.web.hateoas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.junq.examples.usercenter.persistence.model.Role;
+import io.junq.examples.usercenter.service.IRoleService;
+import io.junq.examples.usercenter.util.UserCenter.Privileges;
+import io.junq.examples.usercenter.util.UserCenterMapping;
+
+@Controller
+//@RequestMapping(value = UserCenterMapping.Hateoas.ROLES)
+public class RoleHateoasControllerSimple {
+
+	@Autowired
+	private IRoleService service;
+	
+	// API - 查找单条记录
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    @Secured(Privileges.CAN_ROLE_READ)
+	public RoleResource findOne(@PathVariable("id") final Long id) {
+		final Role entity = service.findOne(id);
+		return new RoleResource(entity);
+	}
+}
